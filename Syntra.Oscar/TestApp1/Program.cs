ab6a60034b1c92011463071adeb8e6fb62943dfb
Micha <micha-vk@hotmail.com>
2019-02-28 20:38:46 +0100

    Added some code to the TestApp1

--- Syntra.Oscar/TestApp1/Program.cs
+++ Syntra.Oscar/TestApp1/Program.cs
@@ -10,6 +10,29 @@ namespace TestApp1
     {
         static void Main(string[] args)
         {
+            int number1 = 0;
+            int number2 = 0;
+            int result = 0;
+
+            for (int i = 0; i < 2; i++)
+            {
+                Console.Write("Please enter a number: ");
+                if (i == 0)
+                {
+                    number1 = int.Parse(Console.ReadLine());
+                }
+
+                if (i == 1)
+                {
+                    number2 = int.Parse(Console.ReadLine());
+                }
+            }
+
+            result = number1 + number2;
+            Console.WriteLine($"Number 1 ({number1}) + number 2 ({number2}) = {result}");
+            Console.ReadKey();
+
+
         }
     }
 }
