39e536e9c9a39f1ac408e915c6557bb67e2d06c2
Olav <olav.hendrickx@hotmail.com>
2019-03-04 18:17:46 +0100

    test olav

---
+++ Syntra.Oscar/TestApp1/Genre.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp1
+{
+    class Genre
+    {
+        public Genre()
+        {
+            Console.WriteLine("test klasse Genre");
+        }
+    }
+}
