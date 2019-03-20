3b846bb99991ee9c6647e74a1aad82a915191b42
Olav <olav.hendrickx@hotmail.com>
2019-03-20 20:09:05 +0100

    de eerste code

---
+++ Syntra.Oscar/Oscar.UI/ConsoleAppScreen.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oscar.UI
+{
+    public class ConsoleAppScreen
+    {
+        public ConsoleAppScreen()
+        {
+
+        }
+
+        public void LoginScreen()
+        {
+            string username;
+            string password;
+
+            bool registratedUser = false;
+
+            Console.WriteLine("Welkom bij Oscar");
+            Console.Write("Ben je al geregistreerd (j/n): ");
+
+            if (Console.ReadLine().ToLower() == "j")
+            {
+                registratedUser = true;
+            }
+
+            if (registratedUser)
+            {
+                Console.Write("Gebruikersnaam: ");
+                username = Console.ReadLine();
+
+                Console.Write("Wachtwoord: ");
+                password = Console.ReadLine();
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
