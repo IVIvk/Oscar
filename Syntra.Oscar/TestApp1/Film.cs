f9fd1468e7bf839aa7fd9948265cdc1f35df6571
Micha <micha-vk@hotmail.com>
2019-03-20 21:48:07 +0100

    Added some data members to the Film class

--- Syntra.Oscar/TestApp1/Film.cs
+++ Syntra.Oscar/TestApp1/Film.cs
@@ -7,8 +7,47 @@ using System.Threading.Tasks;
 namespace TestApp1
 {
     internal class Film
-
     {
-     
+        // Data members.
+        private string filmTitle = string.Empty;
+        private int filmLengthInMinutes = 0;
+        private Genre filmGenre;
+        private int filmRating = 0;
+
+        // Access to the data members.
+        public string FilmTitle
+        {
+            get { return filmTitle; }
+            set { filmTitle = value; }
+        }
+
+        public int FilmLengthInMinutes
+        {
+            get { return filmLengthInMinutes; }
+            set { filmLengthInMinutes = value; }
+        }
+
+        public Genre FilmGenre
+        {
+            get { return filmGenre; }
+            set { filmGenre = value; }
+        }
+
+        public int FilmRating
+        {
+            get { return filmRating; }
+            set { filmRating = value; }
+        }
+
+        // Functions.
+
+        // This function uses user input to fill in the properties of a new film.
+        public void AddNewFilm(string inputOfTitle, int inputOfLength, Genre inputOfGenre)
+        {
+            filmTitle = inputOfTitle;
+            filmLengthInMinutes = inputOfLength;
+            filmGenre = inputOfGenre;
+        }
+
     }
 }
