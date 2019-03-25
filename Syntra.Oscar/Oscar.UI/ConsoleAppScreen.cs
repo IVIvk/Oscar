a62ab1bcff4bf0aa338b03e6e161d4d138498529
Olav <olav.hendrickx@hotmail.com>
2019-03-25 18:01:44 +0100

    Added new user in ConsoleAppScreen

--- Syntra.Oscar/Oscar.UI/ConsoleAppScreen.cs
+++ Syntra.Oscar/Oscar.UI/ConsoleAppScreen.cs
@@ -59,8 +59,53 @@ namespace Oscar.UI
             }
             else
             {
+                User newUser = new User();
+                string newUsername;
+                string newPassword;
+                bool usernameExist = false;
+                bool passwordExist = false;
 
+                do 
+                {
+                    Console.Clear();
+                    usernameExist = false;
+
+                    Console.Write("Geef een gebruikersnaam: ");
+                    newUsername = Console.ReadLine();
+
+                    foreach (User user in userList)
+	                {
+                        if (user.userName == newUsername)
+                        {
+                            usernameExist = true;
+                        }
+	                }
+                }
+                while (usernameExist);
+
+                do
+	            {
+                    Console.Clear();
+                    passwordExist = false;
+
+                    Console.Write("Geef een wachtwoord: ");
+                    newPassword = Console.ReadLine();
+                    Console.Write("Herhaal het wachtwoord: ");
+
+                    if (!(Console.ReadLine() == newPassword))
+	                {
+                        passwordExist = true;
+                        Console.WriteLine("De invoer komt niet overeen. Druk op enter om opnieuw te proberen.");
+                        Console.ReadLine();
+	                }
+	            } while (passwordExist);
+
+                newUser.userName = newUsername;
+                newUser.passWord = newPassword;
+
+                userList.Add(newUser);
             }
+            
         }
     }
 }
