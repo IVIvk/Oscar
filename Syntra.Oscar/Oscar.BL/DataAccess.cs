13c315c06e9783919814f1b1f3f28d199c254f2f
Olav <olav.hendrickx@hotmail.com>
2019-03-27 20:24:10 +0100

    Started WPF login screen

--- Syntra.Oscar/Oscar.BL/DataAccess.cs
+++ Syntra.Oscar/Oscar.BL/DataAccess.cs
@@ -25,9 +25,9 @@ namespace Oscar.BL
                 string temporaryStorage = line;
 
                 user.userName = temporaryStorage.Substring(0, (temporaryStorage.IndexOf('/')));
-                temporaryStorage = temporaryStorage.Substring(1);
+                temporaryStorage = temporaryStorage.Substring(temporaryStorage.IndexOf('/') + 1);
                 user.passWord = temporaryStorage.Substring(0, temporaryStorage.IndexOf('/'));
-                temporaryStorage = temporaryStorage.Substring(1);
+                temporaryStorage = temporaryStorage.Substring(temporaryStorage.IndexOf('/') + 1);
                 user.admin = Convert.ToBoolean(temporaryStorage.Substring(temporaryStorage.IndexOf('/') + 1));
 
                 userList.Add(user);
@@ -36,7 +36,7 @@ namespace Oscar.BL
             return userList;
         }
 
-        public void SaveUsers (List<User> userList, string userFile)
+        public void SaveUsers(List<User> userList, string userFile)
         {
             File.Delete(userFile);
             StreamWriter file = new StreamWriter(File.Create(userFile));
---
+++ Syntra.Oscar/Oscar.UI.WPF/Login.xaml.cs
@@ -0,0 +1,88 @@
+﻿using Oscar.BL;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Oscar.UI.WPF
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Page
+    {
+        List<User> userList;
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+            bool check = true;
+
+            foreach (User user in userList)
+            {
+                if (username == user.userName)
+                {
+                    if (password == user.passWord)
+                    {
+                        check = false;
+                    }
+                }
+            }
+
+            if (check)
+            {
+                MessageBox.Show("Gebruikersnaam of wachtwoord is niet correct");
+            }
+            else
+            {
+                //NavigationService.Navigate();
+            }
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            string userFile = @"c:\temp\usersOscar.txt";
+            userList = new List<User>();
+
+            bool fileExist = File.Exists(userFile);
+
+            if (fileExist) // Checking if userfile exists. If not, creating one with admin-user in it.
+            {
+                userList = new DataAccess().LoadUsers(userFile);
+            }
+            else
+            {
+                string defaultText = @"Admin/AdminPassword/true";
+                StreamWriter file = new StreamWriter(File.Create(userFile));
+                file.Close();
+
+                using (StreamWriter sw = new StreamWriter(userFile))
+                {
+                    sw.WriteLine(defaultText);
+                }
+            }
+        }
+
+        private void btnNewUser_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new PageNewUser(userList));
+        }
+    }
+}
