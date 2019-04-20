8bd8513eb5bd5c435a42203c7c41cbe2c6333fc7
Olav <olav.hendrickx@hotmail.com>
2019-04-20 20:05:32 +0200

    Added UI for Admin tasks

---
+++ Syntra.Oscar/Oscar.UI.WPF/AdminOverview.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Oscar.UI.WPF
+{
+    /// <summary>
+    /// Interaction logic for AdminOverview.xaml
+    /// </summary>
+    public partial class AdminOverview : Page
+    {
+        public AdminOverview()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnUsersOverview_Click(object sender, RoutedEventArgs e)
+        {
+            frmOverviewFrame.NavigationService.Navigate(new AdminUserManagement());
+        }
+    }
+}
