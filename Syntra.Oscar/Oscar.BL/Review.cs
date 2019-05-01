4388c53c72c6c96c2fb387a3ec4d2baab8139393
Olav <olav.hendrickx@hotmail.com>
2019-05-01 17:58:42 +0200

    added class Review and ReviewRepository

---
+++ Syntra.Oscar/Oscar.BL/Review.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oscar.BL
+{
+    public class Review
+    {
+        private Nullable<Guid> reviewId;
+        private string reviewContent;
+        private int reviewScore;
+        private Nullable<Guid> userId;
+
+        public Nullable<Guid> ReviewId
+        {
+            get { return reviewId; }
+            set
+            {
+                if (reviewId == null)
+                {
+                    reviewId = value;
+                }
+            }
+        }
+
+        public string ReviewContent
+        {
+            get { return reviewContent; }
+            set { reviewContent = value; }
+        }
+
+        public int ReviewScore
+        {
+            get { return reviewScore; }
+            set { reviewScore = value; }
+        }
+
+        public Nullable<Guid> UserId
+        {
+            get { return userId; }
+            set
+            {
+                if (userId == null)
+                {
+                    userId = value;
+                }
+            }
+        }
+    }
+}
---
+++ Syntra.Oscar/Oscar.Dapper/Repositories/ReviewRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using Dapper;
+using Oscar.BL;
+
+namespace Oscar.Dapper.Repositories
+{
+    public class ReviewRepository
+    {
+        // SeLect Reviews per User
+        public IEnumerable<Review> GetActors(User user)
+        {
+            using (var connection = new SqlConnection(Connection.Instance.ConnectionString))
+            {
+                return connection.Query<Review>
+                    (@" 
+                        SELECT ReviewId, ReviewContent, ReviewScore, Userid
+                        FROM Reviews
+                        WHERE UserId = @UserId
+                    ", new
+                    {
+                         Userid = user.userId
+                    });
+            }
+        }
+    }
+}
