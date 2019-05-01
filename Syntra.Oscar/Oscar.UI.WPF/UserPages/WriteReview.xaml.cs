47ff8c05127579ad795e68de81c8f7da574fa9a2
Olav <olav.hendrickx@hotmail.com>
2019-05-01 18:05:22 +0200

    Added WPF to write reviews as user

---
+++ Syntra.Oscar/Oscar.UI.WPF/UserPages/WriteReview.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Oscar.UI.WPF.UserPages
+{
+    /// <summary>
+    /// Interaction logic for WriteReview.xaml
+    /// </summary>
+    public partial class WriteReview : Page
+    {
+        public WriteReview()
+        {
+            InitializeComponent();
+        }
+    }
+}
