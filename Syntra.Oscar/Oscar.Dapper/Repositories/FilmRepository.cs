059679f31cbc4341ad36f78af7b40c968f61ff3a
Micha <micha-vk@hotmail.com>
2019-05-31 09:21:22 +0200

    Added query to get a list of films that belong to a certain genre.

--- Syntra.Oscar/Oscar.Dapper/Repositories/FilmRepository.cs
+++ Syntra.Oscar/Oscar.Dapper/Repositories/FilmRepository.cs
@@ -166,6 +166,25 @@ namespace Oscar.Dapper.Repositories
             }
         }
 
+        // This function will return an IEnumerable filled with the films that are of a certain genre.
+        public IEnumerable<Films> GetFilmsOfGenre(Guid genre)
+        {
+            using (var connection = new SqlConnection(Connection.Instance.ConnectionString))
+            {
+                return connection.Query<Films>
+                    (@"
+                        SELECT Films.FilmId , Films.FilmTitle, Films.ReleaseYear, Films.FilmLengthInMinutes, Films.FilmRating, Films.AmountOfRatings, Films.FilmPlot
+                        FROM Films
+                        INNER JOIN GenresInFilms
+                            ON GenresInFilms.FilmId = Films.FilmId
+                        WHERE GenresInFilms.GenreId = @GenreId
+                        ", new
+                    {
+                        GenreId = genre
+                    });
+            }
+        }
+
         // Get the films of a genre. 
         // The below query returns al the films of the input genre.
         //SELECT Films.FilmTitle, Genres.GenreName
